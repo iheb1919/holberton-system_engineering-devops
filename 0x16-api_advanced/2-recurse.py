f5d3b4fab7ba54200d7a232a740b9c0c95039c6a
iheb1919 <ihebmejri14@gmail.com>
2020-05-07 19:53:05 +0100

    test

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Recurse it!
+"""
+
+import requests
+
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    url = 'http://reddit.com/r/{}/hot.json'
+    header = {'User-agent': 'iheb1919'}
+    params = {'t': all, 'after': after}
+    req = requests.get(url.format(subreddit), headers=header,
+                       params=params)
+    print(url.format(subreddit), headers=header,
+                       params=params)
+    if req.status_code != 200:
+        return None
+    for i in req.json()['data']['children']:
+        hot_list.append(i['data']['title'])
+    after = req.json().get('data').get('after')
+    if after:
+        recurse(subreddit, hot_list, after)
+    return hot_list
