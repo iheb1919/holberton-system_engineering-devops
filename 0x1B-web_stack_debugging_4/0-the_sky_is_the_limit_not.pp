ad4f15bde5203393dd0ab5165f0f88d2151d3208
iheb1919 <ihebmejri14@gmail.com>
2020-05-28 17:34:01 +0100

    try2

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
-# fixes limits
-exec {'fix unlimit':
+# The_sky_is_the_limit_not
+exec {'fix':
     command => 'sed -i s/15/1000/ /etc/default/nginx && service nginx restart',
     path    => '/usr/bin/sudo /bin/',
 }
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,2 @@
+# A user_limit
+exec { '/usr/bin/env sed -i "s/holberton/" /etc/security/limits.conf': }
