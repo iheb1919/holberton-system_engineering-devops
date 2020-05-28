a4efb190e535176f93ca08f92aa1e1e0b42a19b5
iheb1919 <ihebmejri14@gmail.com>
2020-05-28 17:21:22 +0100

    try

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,7 @@
-# fix limit
-exec {'fix':
-    command => '/usr/bin/sudo /bin/sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 10000\"/g" /etc/default/nginx',
+# fixes limits
+exec {'fix unlimit':
+    command => 'sed -i s/15/1000/ /etc/default/nginx',
     path    => '/bin',
-    provider => shell,
 }
 exec { 'restart':
   command  => 'sudo service nginx restart',
